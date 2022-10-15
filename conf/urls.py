cc2ddeb29a3eae1feae7d5360569a94db0086d4d
Dmitry <dimachizhikov2@gmail.com>
2022-10-15 14:02:52 +0300

    Добавлена страница с древовидным отображением подразделений и сотрудников

--- conf/urls.py
+++ conf/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from employee_list.views import DepartmentListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', DepartmentListView.as_view(), name='department-list'),
 ]
--- employee_list/views.py
+++ employee_list/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from employee_list.models import Department
+
+
+class DepartmentListView(ListView):
+    model = Department
+    template_name = "employee_list/department_list.html"
