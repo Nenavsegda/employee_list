9aec62e39b46e4bb0292a0db201aa98ab243d7d5
Dmitry <dimachizhikov2@gmail.com>
2022-10-20 17:14:48 +0300

    Добавлен файл миграции

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,5 +4,5 @@
 
 /var/www/Shop-manager/wait-for-it.sh  -h "db" -p "5432" -t 10 -- >&2 echo "PostgreSQL is ready"
 python3 manage.py migrate
-python3 manage.py populatedb
+wait python3 manage.py populatedb
 python3 manage.py runserver 0.0.0.0:8000
